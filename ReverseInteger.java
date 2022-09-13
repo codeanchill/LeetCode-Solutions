20380de8acb858acbeb0b5230f596b4c9615c576
codeanchill <113310527+codeanchill@users.noreply.github.com>
2022-09-13 15:43:32 +0530

    commit

---
+++ ReverseInteger.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        long out=0;
+        while(x!=0){
+            out=out*10 + x % 10;
+            x=x/10;
+        }
+        if(out>Integer.MAX_VALUE||out<Integer.MIN_VALUE) return 0;
+        
+        return (int) out;
+    }
+}
